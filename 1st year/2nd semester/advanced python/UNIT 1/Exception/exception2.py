c5f343150b9ff697b1d295ce1706e1737975dda6
jyotirmaya2004 <jyotirmaybehera2020@gmail.com>
2025-04-20 16:22:45 +0530

    20/4/25

---
+++ 1st year/2nd semester/advanced python/UNIT 1/Exception/exception2.py
@@ -0,0 +1,18 @@
+class myexception(Exception):
+	def __init__(self, args):
+		self.arg=args
+def checkbalance(name,bal):
+	dict={"A":20000,"B":30000,"C":2300,"D":1239}
+	for k,v in dict.items():
+		if name == k:
+			if v < bal:
+				raise myexception("Insufficient balance")
+			else:
+				v -= bal
+				raise myexception("Your withdrawal process is completed successfully.\nYour current balance: %.2f"%v)
+try:
+	name=input("Enter your name : ")
+	bal=int(input("Enter Amount : "))
+	checkbalance(name,bal)
+except myexception as e:
+	print(e)
