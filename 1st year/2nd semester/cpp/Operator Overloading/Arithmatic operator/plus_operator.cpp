20bc1582c669173936c720d5ad276433a08452b3
jyotirmaya2004 <jyotirmaybehera2020@gmail.com>
2025-07-25 19:53:31 +0530

    25/7/25

---
+++ 1st year/2nd semester/cpp/Operator Overloading/Arithmatic operator/plus_operator.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class myclass{
+	public:
+		int a;
+		void getvalue()
+		{
+			cout<<"Enter a number : ";
+			cin>>a;
+		}
+		void putvalue()
+		{
+			cout<<"Your entered value : "<<a<<endl;
+		}
+		void sum()
+		{
+			cout<<"Addition of two object : "<<a<<endl;
+		}
+		myclass operator+(myclass b)
+		{
+			myclass c;
+			c.a=(a+b.a);
+			return c;
+		}
+};
+int main()
+{
+	myclass aa,bb,cc;
+	aa.getvalue();
+	bb.getvalue();
+	cc=aa+bb;
+	aa.putvalue();
+	bb.putvalue();
+	cc.sum();
+
+
+}
---
+++ 1st year/2nd semester/cpp/Operator Overloading/Increment_and_decrement/pre_and_post.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class myclass{
+	public:
+		int a;
+		void getvalue()
+		{
+			cout<<"Enter a number : ";
+			cin>>a;
+		}
+		void putvalue()
+		{
+			cout<<"Your value : "<<a<<endl;
+		}
+		void operator++()
+		{
+			a=a+1;
+		}
+		void operator++(int)
+		{
+			a=a+1;
+		}
+		void operator--()
+		{
+			a=a-1;
+		}
+		void operator--(int)
+		{
+			a=a-1;
+		}
+};
+int main()
+{
+	myclass aa,bb;
+	aa.getvalue();
+	aa.putvalue();
+	++aa;
+	cout<<"After increment : "<<endl;
+	aa.putvalue();\
+	--aa;
+	cout<<"After decrement : "<<endl;
+	aa.putvalue();
+	cout<<endl;
+	bb.getvalue();
+	bb++;
+	cout<<"After increment : "<<endl;
+	bb.putvalue();
+	bb--;
+	cout<<"After decrement : "<<endl;
+	bb.putvalue();
+
+}
