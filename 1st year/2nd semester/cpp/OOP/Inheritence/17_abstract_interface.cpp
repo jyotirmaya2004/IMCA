b2c6cd4e8e1990e667e326c347e3dfc412370ec8
jyotirmaya2004 <jyotirmaybehera2020@gmail.com>
2025-07-31 18:20:02 +0530

    31/7/25

--- 1st year/2nd semester/cpp/OOP/Inheritence/17_abstract_interface.cpp
+++ 1st year/2nd semester/cpp/OOP/Inheritence/17_abstract_interface.cpp
@@ -20,3 +20,18 @@ int main() {
     d.print();
     return 0;
 }
+/*Abstract Class
+🔹 Definition:
+A class is abstract if it has at least one pure virtual function.*/
+
+/*❓ Why can't we create an object of an abstract class in C++?
+Because an abstract class contains at least one pure virtual function, and that function has no body (implementation) in the base class.
+
+⚠️ So what would happen if C++ allowed this?
+If you tried to create an object of such a class:
+
+The compiler wouldn't know which version of the pure virtual function to call.
+
+The class is incomplete — it promises that derived classes will provide the actual implementation.
+
+*/
